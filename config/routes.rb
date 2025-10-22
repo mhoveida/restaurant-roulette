fa7195436f62dc02388426742bdac533511f3ea1
mhoveida <mohaddesehhoveida@yahoo.com>
2025-10-22 14:29:49 -0400

    Remove duplicate root route

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Rails.application.routes.draw do
-  get "static_pages/home"
+  devise_for :users
+  get "home/index"
+  # root to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   root "static_pages#home"
+  get 'solo_spin', to: 'solo_spin#show', as: 'solo_spin'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
